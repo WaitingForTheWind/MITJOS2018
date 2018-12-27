22582b8e3a690e23a0afb69e8cacf3ceb55f8684
LiuJingmeng <ljmnankai@163.com>
2018-12-27 10:34:47 +0800

    Finish the 3rd problem in the lab 2

--- kern/pmap.c
+++ kern/pmap.c
@@ -173,7 +173,9 @@ mem_init(void)
 	//      (ie. perm = PTE_U | PTE_P)
 	//    - pages itself -- kernel RW, user NONE
 	// Your code goes here:
-
+    boot_map_region(kern_pgdir, UPAGES,
+	ROUNDUP((sizeof(struct PageInfo)*npages), PGSIZE),
+	PADDR(pages), (PTE_U | PTE_P));
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
 	// stack.  The kernel stack grows down from virtual address KSTACKTOP.
@@ -185,7 +187,8 @@ mem_init(void)
 	//       overwrite memory.  Known as a "guard page".
 	//     Permissions: kernel RW, user NONE
 	// Your code goes here:
-
+	boot_map_region(kern_pgdir, (KSTACKTOP - KSTKSIZE), 
+	KSTKSIZE, PADDR(bootstack), (PTE_W | PTE_P));
 	//////////////////////////////////////////////////////////////////////
 	// Map all of physical memory at KERNBASE.
 	// Ie.  the VA range [KERNBASE, 2^32) should map to
@@ -194,7 +197,9 @@ mem_init(void)
 	// we just set up the mapping anyway.
 	// Permissions: kernel RW, user NONE
 	// Your code goes here:
-
+	boot_map_region(kern_pgdir, KERNBASE,
+	ROUNDUP((0xFFFFFFFF - KERNBASE), PGSIZE),
+	0, (PTE_W | PTE_P));
 	// Check that the initial page directory has been set up correctly.
 	check_kern_pgdir();
 
